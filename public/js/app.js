b7e8cdb2ce6c8f506c52eb4cabdd7b0fca49f598
Premsagar Gedam <gedamprems@gmail.com>
2023-08-05 03:16:11 +0530

    Added weather api fetch call to app.js client side js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,29 @@
 console.log("Clientside javascript file is loaded!")
+// https://puzzle.mead.io/puzzle
+
+// fetch('https://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=>{
+//         console.log(data)
+//     })
+// })
+
+// Goal : Fetch weather! 
+//
+// 1. Setup a call to fetch weather for Boston
+// 2. Get the parse JSON response
+//    - If error property, print error
+//    - If no error property, print the location and forcast
+// 3. Refresh the browser and test your work 
+
+
+fetch('http://localhost:3000/weather?address=!!').then((response) =>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecastData)
+        }
+        
+    })
+})
