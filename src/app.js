f94dcb428d1dfad89dd011d6230c89541b5ac789
Premsagar Gedam <gedamprems@gmail.com>
2023-08-04 18:22:34 +0530

    Added first lesson: hello express!

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.get('', (req,res)=>{
+    res.send('Hello express!')
+})
+
+app.get('/about', (req,res)=>{
+    res.send('About Page!')
+})
+
+app.get('/help', (req,res)=>{
+    res.send('Help Page!')
+})
+
+app.get('/weather', (req,res) =>{
+    res.send({
+        'name' : 'Premsagar',
+        'age' : 25
+    })
+})
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000!')
+})
