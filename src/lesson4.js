7a18a1c021643ae3266bc9941b0d2dc52350225d
Premsagar Gedam <gedamprems@gmail.com>
2023-08-04 19:08:24 +0530

    Added lesson 4 : serving us css, js, img folder

---
+++ src/lesson4.js
@@ -0,0 +1,5 @@
+// Lesson 4 : Serving up CSS, JS, Images, and More
+
+// added css, js, and img folder in public dir. 
+
+// updated there content access all the files in index.html about.html and help.html
