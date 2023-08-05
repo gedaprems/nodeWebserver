36e1323270bc85e89faa1f4ada2f42893101476e
Premsagar Gedam <gedamprems@gmail.com>
2023-08-05 23:54:28 +0530

    Added the api requirement

---
+++ src/apiToken/apiToken.js
@@ -0,0 +1,9 @@
+const maxboxApiToken = 'pk.eyJ1IjoiZ2VkYW1wcmVtcyIsImEiOiJjbGt2cWUxeTIwdWMyM25vNGZxb2x3M2tkIn0.aWIJrmYRBoSvxvDK84mcIg'
+const weatherStackApiToken = '3ff8ec6c57c170aa33e3d6fc410643c6'
+
+// exporting the values
+
+module.exports = {
+    maxboxApiToken : maxboxApiToken,
+    weatherStackApiToken: weatherStackApiToken
+}
