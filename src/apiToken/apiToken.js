c4dab044f5caefe8cb7633f0483e097e8eac5a7e
Premsagar Gedam <gedamprems@gmail.com>
2023-08-06 00:07:12 +0530

    Trying one more time

--- src/apiToken/apiToken.js
+++ src/apiToken/apiToken.js
@@ -1,7 +1,7 @@
 const maxboxApiToken = 'pk.eyJ1IjoiZ2VkYW1wcmVtcyIsImEiOiJjbGt2cWUxeTIwdWMyM25vNGZxb2x3M2tkIn0.aWIJrmYRBoSvxvDK84mcIg'
 const weatherStackApiToken = '3ff8ec6c57c170aa33e3d6fc410643c6'
 
-// exporting the values
+// exporting the values.
 
 module.exports = {
     maxboxApiToken : maxboxApiToken,
