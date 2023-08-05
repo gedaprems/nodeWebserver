22cac7f22646562c1cf77adde63a73381c52265c
Premsagar Gedam <gedamprems@gmail.com>
2023-08-05 23:51:55 +0530

    Update required file for production ready setup

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e)=>{
     message1.textContent = 'Loading...'
     message2.textContent = ''
 
-    const url = 'http://localhost:3000/weather?address='+location
+    const url = '/weather?address='+location
 
     fetch(url).then((response) =>{
         response.json().then((data)=>{
--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
+
 const app = express()
+const port = process.env.PORT || 3000
+
 
 // Define path for Express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -82,6 +86,6 @@ app.get('/*', (req,res) =>{
 })
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is up on port 3000!')
 })
